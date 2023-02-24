a81586dc095333d58bf500c7e8c26ff8e1c8ccbc
theesmart <samkinyua7556@gmail.com>
2023-02-24 10:47:56 -0800

    0-main.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,11 @@
 #include "main.h"
 
 /**
- * _isupper -  checks for uppercase character.
- * @c: c -  Variable
- * Return: Always 0.
+ * _isupper - checks for uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
-
-	if (c >= 65 && c <= 90)
-	{
-		return (1);
-	}
-	return (0);
+	return (c >= 'A' && c <= 'Z');
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,16 +1,11 @@
-#include "main.h"
-#include <stdio.h>
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-int convert_day(int month,int day);
-void print_remaining_days(int month,int day,int year);
-void print_to_98(int n);
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-int _putchar(char c);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
@@ -19,4 +14,4 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 
-#endif 
+#endif

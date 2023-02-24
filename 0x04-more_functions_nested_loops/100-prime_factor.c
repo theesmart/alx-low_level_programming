b56338fe3c4204804662d3b05e170e1aca935ecf
theesmart <samkinyua7556@gmail.com>
2023-02-24 11:59:16 -0800

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
+
 /**
- * main - prints prime factors of 612852475143
+ * main -  checks for checks for a digit (0 through 9).
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
+	long x, y;
+
+	x = 612852475143;
+
+	for (y = 2; x > y; y++)
+	{
+		while (x % y == 0)
+		{
+			x = x / y;
+		}
+	}
+	printf("%lu", y);
+	putchar('\n');
 	return (0);
 }

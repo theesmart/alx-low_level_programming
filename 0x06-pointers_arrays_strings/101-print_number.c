dd248074ab268f25af88d14bbade911695c57767
theesmart <samkinyua7556@gmail.com>
2023-03-02 05:38:32 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,21 +1,24 @@
 #include "main.h"
 
 /**
- * print_number -print number putchar
- * @n:integer
- * Return: void
+ * print_number - prints an integer;
+ * @n: integer to be printed;
  */
 void print_number(int n)
 {
-	unsigned int x = n;
+	unsigned int n1;
 
 	if (n < 0)
 	{
+		n1 = -n;
 		_putchar('-');
-		x = -x;
+	} else
+	{
+		n1 = n;
 	}
-	if ((x / 10) > 0)
-		print_number(x / 10);
 
-	_putchar(x % 10 + '0');
+	if (n1 / 10)
+		print_number(n1 / 10);
+
+	_putchar((n1 % 10) + '0');
 }
